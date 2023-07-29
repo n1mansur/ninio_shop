c31d55339372090fed817bd9df83461e836ebe89
n1mansur <akkauntforit@gmail.com>
2023-07-29 11:38:35 +0500

    test discount

--- src/components/Pages/Main/components/Products/index.jsx
+++ src/components/Pages/Main/components/Products/index.jsx
@@ -4,21 +4,22 @@ import { Box, SimpleGrid } from '@chakra-ui/react'
 import ProductCard from '@/components/UI/ProductCard'
 import Link from 'next/link'
 import { discountsService } from '../../../../../services/discountsService'
+import DiscountProductCard from '../../../../UI/DiscountProductCard'
 
 export default function Products({ data }) {
   const products = data.slice(0, 8)
   const [dis, setDis] = useState([]);
-useEffect(() => {
-  discountsService?.getList({ data: { with_relations: true }}).then(res => setDis(res.data.response))
-}, []);
-
+  useEffect(() => {
+    discountsService?.getList({ data: { with_relations: true } }).then(res => setDis(res.data.response))
+  }, []);
+  //console.log(dis);
   return (
     <Box mb={'24px'}>
       <SimpleGrid columns={[1, 2, 3, 4]} spacing={'20px'} className={styles.products}>
         {dis && dis.map((el) => {
-          return el.status ? (
-            <ProductCard el={el} key={el.guid} />
-          ) : null
+          return (
+            <DiscountProductCard el={el.products_id_data} discount={el} key={el.guid} />
+          )
         })}
       </SimpleGrid>
       <SimpleGrid columns={[1, 2, 3, 4]} spacing={'20px'} className={styles.products}>
---
+++ src/components/UI/DiscountProductCard/index.jsx
@@ -0,0 +1,95 @@
+import React, { useEffect, useState } from 'react'
+import { Box, Button, Image, Text, Tooltip } from '@chakra-ui/react'
+import Link from 'next/link'
+import { HeartIcon } from '@/components/svg'
+import styles from './styles.module.scss'
+
+export default function DiscountProductCard({ el, discount }) {
+  const [tofavorite, setTofavorite] = useState([]);
+
+  function checkDate(dateStr) {
+    const today = new Date()
+    const [year, month, day] = dateStr.split('-')
+    const date = new Date(year, month - 1, day)
+    const diff = Math.floor((today - date) / (1000 * 60 * 60 * 24))
+    return diff < 7
+  }
+  console.log(discount);
+
+  //useEffect(() => {
+  //  let tofavoriteProducts = JSON.parse(localStorage.getItem('toFavorites')) || []
+  //  setTofavorite(tofavoriteProducts)
+  //}, []);
+
+  //const toFavoritesProduct = (el) => {
+  //  let tofavoriteProducts = JSON.parse(localStorage.getItem('toFavorites')) || []
+  //  const find = tofavoriteProducts.find(old => old === el.guid)
+  //  find
+  //    ? tofavoriteProducts = tofavoriteProducts.filter(old => old != el.guid)
+  //    : tofavoriteProducts.push(el.guid)
+  //  setTofavorite(tofavoriteProducts)
+  //  localStorage.setItem('toFavorites', JSON.stringify(tofavoriteProducts))
+  //}
+
+  //const toggle = tofavorite?.filter(old => old === el.guid)
+
+  const oldPriceFormatted = discount?.old_price?.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1 ');
+  const newPriceFormatted = discount?.new_price?.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1 ');
+
+  return (
+    <Box onClick={() => {
+      window.location.href = `/product/${el.guid}`
+    }}
+      w={'auto'}
+      h={'auto'}
+    >
+      <Box
+        className={styles.product} key={el.guid}
+      >
+      <span className={styles.discountStyle}>Акция</span>
+        <Box className={styles.heroSection}>
+          <Box
+            w={'100%'}
+            display={'flex'}
+            justifyContent={'space-between'}
+            alignItems={'center'}
+            flexDir={'row-reverse'}
+            position={'relative'}
+            zIndex={'10'}
+          >
+            {/*<Button className={styles.likedBtn} onClick={(e) => {
+              e.stopPropagation()
+              toFavoritesProduct(el)
+            }}>
+              <HeartIcon bg={toggle?.length > 0 ? '#84919A' : 'none'} />
+            </Button>*/}
+            {
+              checkDate(el.create_date.slice(0, 10)) ?
+                <Text className={styles.status}>Новинка</Text> : null
+            }
+          </Box>
+          <Box
+            position={'relative'}
+            zIndex={'1'}
+            className={styles.cardImg}
+          >
+            <Image src={el.photo} alt={el.name} />
+          </Box>
+        </Box>
+        <Box className={styles.productInfo}>
+          <Text className={styles.productCategory}>
+            {el.categories_id_data.name}
+          </Text>
+          <Text className={styles.productName}>
+            <Tooltip label={el.name}  aria-label='A tooltip'>
+              <span>{el.name}</span>
+            </Tooltip>
+          </Text>
+          <Text className={styles.oldPrice}>не {oldPriceFormatted} Сум</Text>
+          <Text className={styles.newPrice}>{newPriceFormatted} Сум</Text>
+        </Box>
+        <Button className={styles.buyBtn}>Добавить</Button>
+      </Box>
+    </Box>
+  )
+}

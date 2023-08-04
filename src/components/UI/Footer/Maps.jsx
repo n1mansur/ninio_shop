ab5b3ce01a6951a7dd11b8829176ada85175e366
n1mansur <akkauntforit@gmail.com>
2023-08-04 22:23:12 +0500

    fff

--- src/components/UI/Footer/Maps.jsx
+++ src/components/UI/Footer/Maps.jsx
@@ -1,7 +1,33 @@
 import React from 'react'
+import {
+  YMaps,
+  Map,
+  FullscreenControl,
+  Placemark,
+}
+  from '@pbe/react-yandex-maps';
+const API_KEY = '1b0887e8-557b-479a-a776-134a8698943c'
 
 export default function Maps() {
+  const defaultState = {
+    center: [41.2924481, 69.2116919],
+    zoom: 16,
+  };
+  //const mapOptions = {
+  //  suppressMapOpenBlock: true
+  //};
+
   return (
-    <iframe src="https://yandex.ru/map-widget/v1/?um=constructor%3A5e43476bc7978b2ec59e7cbf423960556f037ef500c36c8243e9564db8a9e502&amp;source=constructor" width="100%" height="400" frameBorder="0"></iframe>
+    <YMaps query={{ lang: 'ru_RU', apikey: API_KEY }}>
+      <Map
+        defaultState={defaultState}
+        width={'100%'}
+        //options={mapOptions}
+      >
+        <FullscreenControl />
+        <Placemark geometry={[41.2924481, 69.2116919]} />
+      </Map>
+    </YMaps>
+    //<iframe className={styles.maps} src="https://yandex.ru/map-widget/v1/?um=constructor%3A5e43476bc7978b2ec59e7cbf423960556f037ef500c36c8243e9564db8a9e502&amp;source=constructor&controls=-geolocationControl" width="100%" height="400" frameBorder="0"></iframe>
   )
 }
